04674bd0e7c666ddbd19af3478a2bf5863c5f198
reynerdev <rloza@pucp.pe>
2021-05-24 12:07:15 -0500

    before trip arequipa

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,2 +1,5 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import React from 'react';
 export default () => <h1>Index Page</h1>;
--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,5 +1,8 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import React from 'react';
-
+import Image from 'next/image';
 const request = {
   arrangeDate: '12/13/2012',
   downPayment: 130,
@@ -8,8 +11,37 @@ const request = {
   description: 'Build a ecommerce about clothing ',
 };
 
+const props = {
+  logosrc: '#',
+  title: 'Web design ecommerce project',
+  description: 'Design a complete ecommerce with a backend capabilities.',
+  clientInfo: 'ClientInfoObject',
+  requestinfo: 'RequestFetchedInfo',
+  downPayment: 123,
+  totalPayment: 150,
+  status: 'Pending client payment',
+};
+
 const Request = () => {
-  return <div> dasdasd </div>;
+  return (
+    <div>
+      <div className="Logo">
+        <Image
+          src="/images/profile.jpg"
+          width={50}
+          height={50}
+          alt="Picture of the author"
+          sx={{ borderRadius: '50%' }}
+        />
+      </div>
+      <div className="middlePart">
+        <div className="Title">Web design ecommerce project</div>
+        <div className="description"> Trying t</div>
+      </div>
+      <div className="downPayment">props.downPayment</div>
+      <div className="totalPayment">props.totalPayment</div>
+    </div>
+  );
 };
 
 export default Request;
